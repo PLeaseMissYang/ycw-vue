7293f8fe12da43c3ccc8ccb70c211dba8cdec956
pleasemissyang <863641231@qq.com>
2019-12-17 11:45:00 +0800

    second commit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,4 +1,5 @@
 <template>
+  <body id="poster">
   <el-form class="login-container" label-position="left"
            label-width="0px">
     <h3 class="login_title">系统登录</h3>
@@ -14,39 +15,8 @@
       <el-button type="primary" style="width: 100%;background: #505458;border: none" v-on:click="login">登录</el-button>
     </el-form-item>
   </el-form>
+  </body>
 </template>
-
-<style>
-  .login-container {
-    border-radius: 15px;
-    background-clip: padding-box;
-    margin: 90px auto;
-    width: 350px;
-    padding: 35px 35px 15px 35px;
-    background: #fff;
-    border: 1px solid #eaeaea;
-    box-shadow: 0 0 25px #cac6c6;
-  }
-
-  .login_title {
-    margin: 0px auto 40px auto;
-    text-align: center;
-    color: #505458;
-  }
-
-  #poster {
-    background:url("../assets/non.jpg") no-repeat;
-    background-position: center;
-    height: 100%;
-    width: 100%;
-    background-size: cover;
-    position: fixed;
-  }
-  body{
-    margin: 0px;
-  }
-
-</style>
 
 <script>
 
@@ -55,8 +25,8 @@
     data () {
       return {
         loginForm: {
-          username: '',
-          password: ''
+          username: 'admin',
+          password: '123'
         },
         responseResult: []
       }
@@ -79,4 +49,34 @@
     }
   }
 </script>
+
+<style>
+  #poster {
+    background:url("../assets/non.jpg") no-repeat;
+    background-position: center;
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+    position: fixed;
+  }
+  body{
+    margin: 0px;
+  }
+  .login-container {
+    border-radius: 15px;
+    background-clip: padding-box;
+    margin: 90px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+  }
+  .login_title {
+    margin: 0px auto 40px auto;
+    text-align: center;
+    color: #505458;
+  }
+
+</style>
 
